f5aa9bfeb9a39b54f869b3e804eefa15f6dc31c5
gatchigat <80865346+gatchigat@users.noreply.github.com>
2022-04-10 14:35:55 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,3 +19,35 @@ $(".navbar-collapse").on("shown.bs.collapse", function () {
 $(".navbar-collapse").on("hidden.bs.collapse", function () {
   $(".navbar-collapse  >.collapse.m-0").removeClass("mt-3 mb-3");
 });
+function onBeforeUnload(e) {
+  if (thereAreUnsavedChanges()) {
+    e.preventDefault();
+    e.returnValue = "wakoko";
+    return;
+  }
+
+  delete e["returnValue"];
+}
+
+window.addEventListener("beforeunload", onBeforeUnload);
+// window.onbeforeunload = function (e) {
+//   e = e || window.event;
+//   alertString = "Awit wakokohaha";
+//   // For IE and Firefox prior to version 4
+//   if (e) {
+//     e.returnValue = "wrawrawrwar";
+//   }
+
+//   // For Safari
+//   return "wrwarawraw";
+// };
+
+function unloadConfirm() {
+  window.onbeforeunload = function (e) {
+    var e = e || window.event;
+    if (e) {
+      e.returnValue = "You turned me on.";
+    }
+    return "You turned me on.";
+  };
+}
